60c464663eaf223c0d53e8378d81f5e3cfe20ed3
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-01-09 06:35:45 +0000

    Add

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,28 +1,56 @@
 #!/usr/bin/python3
 """Define module to check boxes keys"""
+# def canUnlockAll(boxes: list) -> bool:
+#     """
+#     Check if the all boxes have keys
+
+#     Arguments:
+#         boxes: list contains boxes with its keys
+
+#     Returns:
+#         true if all boxes have keys else false
+#     """
+#     current_keys = [key for key in boxes[0]]
+#     seen_boxes = {0}
+
+#     while current_keys:
+#         current_key = current_keys.pop(0)
+#         if current_key in seen_boxes:
+#             continue
+
+#         current_keys += [key for key in boxes[current_key]]
+#         seen_boxes.add(current_key)
+
+#     print(seen_boxes)
+
+#     return len(seen_boxes) == len(boxes)
 
 
-def canUnlockAll(boxes: list) -> bool:
-    """
-    Check if the all boxes have keys
-
-    Arguments:
-        boxes: list contains boxes with its keys
-
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened or not
     Returns:
-        true if all boxes have keys else false
+        True: all boxes can be opened
+        False: not all boxes can be opened
     """
-    keys = {0}
-    current_keys = [key for key in boxes[0]]
-    seen_boxes = {0}
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    while current_keys:
-        current_key = current_keys.pop(0)
-        if current_key in seen_boxes:
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
             continue
+        else:
+            break
 
-        current_keys += [key for key in boxes[current_key]]
-        keys.add(current_key)
-        seen_boxes.add(current_key)
-
-    return len(keys) == len(boxes)
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
